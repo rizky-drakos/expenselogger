9175f62362b47845b0937ac6bb3c8b87ab8820e2
Hong-Phat Nguyen <nhphat1997@gmail.com>
2024-01-05 02:31:34 +0700

    Provision local DynamoDB

---
+++ infra/kind/dynamodb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+trap cleanup ERR
+
+function cleanup {
+  echo "Cleaning up!"
+  kubectl delete ns/dynamodb
+}
+
+case $1 in
+  "on")
+    kubectl create ns dynamodb
+    kubectl apply -f templates/dynamodb.yaml
+    kubectl wait --all --for=condition=Ready -l app=dynamodb pod -n dynamodb --timeout=60s
+    kubectl apply -f templates/create-table.yaml
+    kubectl wit --all --for=condition=Complete=True job -n dynamodb --timeout=60s
+    ;;
+  "off")
+    cleanup
+    ;;
+  *)
+    echo "[ERR] Accept only: deploy-dynamodb on|off"
+    ;;
+esac
